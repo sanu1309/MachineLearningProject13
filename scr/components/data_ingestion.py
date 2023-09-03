098387ecb3ec5870fc19d43bcf475cb80c6195a6
sanu1309 <suprakashsaha.13@gmail.com>
2023-09-03 20:37:15 +0530

    Model Trainer

--- scr/components/data_ingestion.py
+++ scr/components/data_ingestion.py
@@ -6,8 +6,12 @@ import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
 from scr.components.data_transformation import DataTransformation
 from scr.components.data_transformation import DataTransformationConfig
+
+from scr.components.model_trainer import ModelTrainerConfig
+from scr.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -22,7 +26,7 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('Notebook\data\stud.csv')
+            df=pd.read_csv('notebook\data\stud.csv')
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
@@ -49,7 +53,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
-    
-    data_transformation= DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
 
+    data_transformation=DataTransformation()
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
