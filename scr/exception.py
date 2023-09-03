2a202fcff07577a7526fa40075ff284cb598ec5c
sanu1309 <suprakashsaha.13@gmail.com>
2023-09-03 14:07:45 +0530

    EDA and Problem Statement

--- scr/exception.py
+++ scr/exception.py
@@ -1,4 +1,5 @@
 import sys
+from scr.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -11,10 +12,19 @@ def error_message_detail(error,error_detail:sys):
 
 
 
-class customException(Exception):
+class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message_detail(error_message,error_detail)
 
     def __str__(self):
         return self.error_message
+    
+
+if __name__=="__main__":
+
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Devide by Zero")
+        raise CustomException(e,sys)
